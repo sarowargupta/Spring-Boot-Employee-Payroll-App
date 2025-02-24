75159b7c43767e779cd8541c0ae4f862c7b2e214
sarowargupta <sarowargupta2001@gmail.com>
2025-02-24 23:01:32 +0530

    [Sarowar Gupta]add. Spring Employee Payroll App Development.

---
+++ src/main/java/com/employee/payrollappdevelopment/PayrollappdevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.employee.payrollappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollappdevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollappdevelopmentApplication.class, args);
+	}
+
+}
