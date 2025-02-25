9cba8c9f6719be283f2a07531bbdbc5313690916
sarowargupta <sarowargupta2001@gmail.com>
2025-02-25 12:05:07 +0530

    [Sarowar Gupta]add. Employee Payroll App Development using Lombok Library.

--- src/main/java/com/employee/payrollappdevelopment/dto/EmployeeDTO.java
+++ src/main/java/com/employee/payrollappdevelopment/dto/EmployeeDTO.java
@@ -2,29 +2,19 @@ package com.employee.payrollappdevelopment.dto;
 
 // Section:-02 UC -01 Introducing DTO class
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Section :-03 Application Setting & UC-01 lombok library to autogenerate
+//getter and setter for the DTO
+
 //Employee DTO class
+@Data // Generate getters, setters method
+@NoArgsConstructor // lombok generate no argument constructor
+@AllArgsConstructor // Generate a constructor with argument of all field
 public class EmployeeDTO {
     private String name;
     private double salary;
 
-    public EmployeeDTO() {}
-
-    public EmployeeDTO(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
